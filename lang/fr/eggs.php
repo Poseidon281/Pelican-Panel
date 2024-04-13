c6ce1b64d37e4d92bcaf1725340db8fb8f4b77ac
Poseidon <62171904+Poseidon281@users.noreply.github.com>
2024-04-13 18:38:31 +0200

    New translations eggs.php (French)

--- lang/fr/eggs.php
+++ lang/fr/eggs.php
@@ -2,9 +2,9 @@
 
 return [
     'notices' => [
-        'imported' => 'Successfully imported this Egg and its associated variables.',
+        'imported' => 'Cet Egg et ses variables ont été importés avec succès.',
         'updated_via_import' => 'This Egg has been updated using the file provided.',
-        'deleted' => 'Successfully deleted the requested egg from the Panel.',
+        'deleted' => 'L\'Egg demandé a été supprimé du panneau.',
         'updated' => 'Egg configuration has been updated successfully.',
         'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
         'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
