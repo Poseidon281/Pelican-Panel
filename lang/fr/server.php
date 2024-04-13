4b22f2514b9261bf57f177fa600c0988a81ca0d4
Poseidon <62171904+Poseidon281@users.noreply.github.com>
2024-04-13 18:38:38 +0200

    New translations server.php (French)

--- lang/fr/server.php
+++ lang/fr/server.php
@@ -2,7 +2,7 @@
 
 return [
     'exceptions' => [
-        'no_new_default_allocation' => 'You are attempting to delete the default allocation for this server but there is no fallback allocation to use.',
+        'no_new_default_allocation' => 'Vous essayez de supprimer l\'allocation par défaut pour ce serveur, mais il n\'y a pas d\'allocation de secours à utiliser.',
         'marked_as_failed' => 'This server was marked as having failed a previous installation. Current status cannot be toggled in this state.',
         'bad_variable' => 'There was a validation error with the :name variable.',
         'daemon_exception' => 'There was an exception while attempting to communicate with the daemon resulting in a HTTP/:code response code. This exception has been logged. (request id: :request_id)',
@@ -16,12 +16,12 @@ return [
         'suspension_toggled' => 'Server suspension status has been changed to :status.',
         'rebuild_on_boot' => 'This server has been marked as requiring a Docker Container rebuild. This will happen the next time the server is started.',
         'install_toggled' => 'The installation status for this server has been toggled.',
-        'server_reinstalled' => 'This server has been queued for a reinstallation beginning now.',
-        'details_updated' => 'Server details have been successfully updated.',
+        'server_reinstalled' => 'Ce serveur a été mis en file d\'attente pour le début de la réinstallation.',
+        'details_updated' => 'Les détails du serveur ont été mis à jour avec succès.',
         'docker_image_updated' => 'Successfully changed the default Docker image to use for this server. A reboot is required to apply this change.',
         'node_required' => 'You must have at least one node configured before you can add a server to this panel.',
         'transfer_nodes_required' => 'You must have at least two nodes configured before you can transfer servers.',
-        'transfer_started' => 'Server transfer has been started.',
-        'transfer_not_viable' => 'The node you selected does not have the required disk space or memory available to accommodate this server.',
+        'transfer_started' => 'Le transfert du serveur a été démarré.',
+        'transfer_not_viable' => 'Le nœud que vous avez sélectionné ne dispose pas de l\'espace disque ou de la mémoire nécessaire pour accueillir ce serveur.',
     ],
 ];
